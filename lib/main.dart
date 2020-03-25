0cf8894d4535d5043f5eb058a5d5a585c8e6958d
ricardohsmello <ricardo.mello@senior.com.br>
2020-03-25 02:06:10 -0300

    Creating form view

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,116 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(title: const Text('Bytebank')),
-      body: ListaTransferencia(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
-    )));
+void main() => runApp(ByteBankApp());
 
-class ListaTransferencia extends StatelessWidget {
+class ByteBankApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        ItemTransferencia(Transferencia('Caixa', 120, '01-20-2020')),
-        ItemTransferencia(Transferencia('Santander', 3120, '01-20-2020')),
-        ItemTransferencia(Transferencia('Nubank', 120, '01-20-2020')),
-
-      ],
+    return MaterialApp(
+      home: Scaffold(
+        body: TransferForm(),
+      ),
     );
   }
 }
 
-class ItemTransferencia extends StatelessWidget {
+class TransferForm extends StatelessWidget {
+  final TextEditingController _controllerAccountName = TextEditingController();
+  final TextEditingController _controllerAccountValue = TextEditingController();
 
-  final Transferencia _transferencia;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New account bank'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controllerAccountName,
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                  labelText: 'Account name', hintText: 'CitiBank'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controllerAccountValue,
+              style: TextStyle(fontSize: 24.0),
+              decoration: InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  labelText: 'Opening balance',
+                  hintText: '2000'),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          RaisedButton(
+            child: Text('Salvar'),
+            onPressed: () {
+              final String accountName = _controllerAccountName.text;
+              final double accountValue =
+                  double.tryParse(_controllerAccountValue.text);
 
-  ItemTransferencia(this._transferencia);
+              if (accountName != null && accountValue != null) {
+                final transfCreated = Transfer(accountName, accountValue);
+                debugPrint('$transfCreated');
+
+                Scaffold.of(context).showSnackBar(SnackBar(
+                  content: Text('Record saved successfully!'),
+                ));
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TransferList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Transfer List')),
+      body: Column(
+        children: <Widget>[
+          TransferItem(Transfer('CitiBank', 2120)),
+          TransferItem(Transfer('Santander', 312)),
+          TransferItem(Transfer('Nubank', 720)),
+        ],
+      ),
+    );
+  }
+}
+
+class TransferItem extends StatelessWidget {
+  final Transfer _transfer;
+
+  TransferItem(this._transfer);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
         leading: Icon(Icons.monetization_on),
-        title: Text(_transferencia.nomeConta),
-        subtitle: Text(_transferencia.valorConta.toString()),
-        trailing: Text(_transferencia.data),
+        title: Text(_transfer.accountName),
+        subtitle: Text(_transfer.accountValue.toString()),
+        trailing: Text('25/03/2020'),
       ),
     );
   }
 }
 
-class Transferencia {
-  String nomeConta;
-  double valorConta;
-  String data;
+class Transfer {
+  String accountName;
+  double accountValue;
 
-  Transferencia(this.nomeConta, this.valorConta, this.data);
+  Transfer(this.accountName, this.accountValue);
+
+  @override
+  String toString() {
+    return 'Transfer{accountName: $accountName, accountValue: $accountValue}';
+  }
 }
